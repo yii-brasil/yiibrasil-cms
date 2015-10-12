1c8c13357074cd5c30b72429d52852ecf358b796
Cálcio <calcionit@gmail.com>
2015-10-12 19:19:56 -0300

    teste

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yiicms',
     'username' => 'root',
-    'password' => 'BdLnx#1404',
+    'password' => '',
     'charset' => 'utf8',
 ];
