89e300df236abb6bcfb034f3966f87b39e586a74
Calcio <calcionit@gmail.com>
2015-11-10 15:01:33 +0000

    modificação da senha

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yiicms',
     'username' => 'root',
-    'password' => 'serpros01',
+    'password' => '',
     'charset' => 'utf8',
 ];
