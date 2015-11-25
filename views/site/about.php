6f61e2b11b4823996994821b88ebd1ac398e06ed
Emanuel A. Leite <emanuelandrade15@gmail.com>
2015-11-25 18:29:32 -0300

    View do 'site/about.php' simplificada

--- views/site/about.php
+++ views/site/about.php
@@ -9,42 +9,33 @@ $this->title = 'Sobre';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <?php // Apenas o usuário logado poderá visualizar o botão.
-    if (!Yii::$app->user->isGuest) {
-        ?>
+    <?php if (!Yii::$app->user->isGuest) : ?>
         <div class="pull-right">
             <?= Html::a('Editar conteúdo', ['about-edit'], ['class' => 'btn btn-default']) ?>
         </div>
         <div class="clearfix"></div>
-        <?php
-    } ?>
+    <?php endif; ?>
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php if (isset($model) && $model->status == $model::STATUS_ATIVO) {
-        echo htmlspecialchars_decode($model->conteudo, ENT_QUOTES);
-    } elseif (isset($model) && $model->status == $model::STATUS_INATIVO) {
-        // Apenas o usuário logado poderá visualizar esta mensagem.
-        if (!Yii::$app->user->isGuest) {
-            ?>
+    <?php if (isset($model) && $model->status == $model::STATUS_ATIVO) : ?>
+        <?= htmlspecialchars_decode($model->conteudo, ENT_QUOTES) ?>
+    <?php elseif (isset($model) && $model->status == $model::STATUS_INATIVO) : ?>
+        <?php if (!Yii::$app->user->isGuest) : ?>
             <p class="alert alert-warning">
-                O conteúdo desta página foi desativado. Para ativá-lo, clique no botão <strong>Editar conteúdo</strong> e altere o status para <strong>ativo</strong>.
+                O conteúdo desta página foi desativado. Para ativá-lo, clique no
+                botão <strong>Editar conteúdo</strong> e altere o status para
+                <strong>ativo</strong>.
             </p>
-
             <?= htmlspecialchars_decode($model->conteudo, ENT_QUOTES) ?>
-            <?php
-        } else {
-            ?>
+        <?php else : ?>
             <p>
                 Nenhum conteúdo foi adicionado nesta página!
             </p>
-            <?php
-        }
-    } else {
-        ?>
+        <?php endif; ?>
+    <?php else : ?>
         <p>
             Nenhum conteúdo foi adicionado nesta página!
         </p>
-        <?php
-    } ?>
+    <?php endif; ?>
 </div>
